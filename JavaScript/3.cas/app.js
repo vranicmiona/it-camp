50d06c9c98ba27cc6b7dc978e9834d8dd7d4d104
vranicmiona <111905437+vranicmiona@users.noreply.github.com>
2022-10-18 21:12:45 +0200

    OPERATORI U JAVA SCRIPTU

---
+++ JavaScript/3.cas/app.js
@@ -0,0 +1,58 @@
+// 1. var
+var x; // predstavlja deklarisanje promenljive x, obezbedjivanje lokacijske memorije za promenljivu x
+x = 15; // vrednost koju smo stavili na tu lokacijsku memoriju (inicijalizacija promenljive x, dodela vrednosti vec deklarsanoj promenljivoj x)
+// var x = 15 - takodje dozvoljeno
+console.log(x); // promenljiva nijen unutar navodnika(
+// karakteristicno za var je da moze da se izvrsi redeklarisana za taj keyword, i to bilo gde u prostoru.
+var x;
+// Reinicijalizacija promenljive x putem var keyword je dozvoljeno bilo gde  prostoru.
+x = 22;
+
+// 2. let - definisanje promenljive za koju postoji mogucnost promene vrednosti 
+let y;
+y = 14;
+console.log(y);
+// let y = 14; - takodje dozvoljeno i sve vazi ako za var definice samo menjamo "y".
+
+// Redeklarisanje definisane putem let keyword nije dozvoljeno ako se radi o istom prostoru.
+
+
+y = 12; // Reinicijalizacija promenljive definisane putem let keyworda je dozvoljeno u bilo kojem prostoru.
+ console.log(y); // dek -davanje promenljive  inc - davanje vrdnosti
+
+//  Promenljive koje su definisane putem let ili const keyword  nam obezbedjuju Block Scope.
+{
+    // code koji se naalzi u Block Scope-u.
+    // console.log(y); // logika je da hoce da bude prepoznata promenljive, jer smo napravili podprostor Global Scope-a.
+    // Redeklaracija promenljive definisane putem let u drugom prostoru dozvoljena.
+    let y = 16;
+    console.log(y);
+
+    // let a, b, c; - korektan ancin definisanja promenljivih
+
+}
+
+console.log(y); // vracnje nazad daje istu vrednost kao iz Global Scope-a.
+
+// 3. Const - ne da nam da je menjamo do samoga kraja.
+// Promenljive definisane putem const moraju biti odjednom deklarisane i inicijalizovane.
+// const z;
+// z = 26;
+const z = 26;
+console.log(z);
+// NIJE DOZVOLJENA NI REDEKLARACIJA, NI REINICIJALIZACIJA.
+
+// const.log(z); - ne moze da se deklarise vise puta, tj redeklarisane promenljive deklarisaen putem const nije dozvoljeno kada se radi o istom prostoru.
+
+// z = 23;
+// console.log(z); - reinicaizacija definisane promenljive putem const nije dozvoljena, kada se radi o istom prostru.
+
+
+
+
+
+
+
+
+
+
---
+++ JavaScript/3.cas/app2.js
@@ -0,0 +1,60 @@
+// OPERATORI U JAVA SCRIPT-U //
+// Podelicemo operatore u sets grupa:
+// 1. Aritmeticki operatori(Aritmetic operators);
+// 2. Opertatori dodele vrednosti (Assigment operators);
+// 3. String operatori (String operators);
+// 4. Operatori poredjenja (Comperison operators);
+// 5. Logicki operatori (Logical operators);
+// 6. Tip operatora (Type operators);
+
+// 1. Aritmeticki operatori(Aritmetic operators);
+// 1.1  +   operator 
+console.log(4 + 5);
+// 1.2 - odeuzimanje
+console.log(4 - 5);
+// 1.3 * mnozenje
+console.log(4 * 5);
+// 1.4 / deljenje
+console.log(4 / 5);
+// 1.5 ** operator stepenovanja
+console.log(2 ** 3);
+// 1.6 % Modulo (ostatak pri deljenju)
+console.log(11 % 3); // 2
+console.log(12 % 3); // 0
+
+// Uslov za parnost:
+// i % 2 = 0
+// Uslov za neparnost:
+// i % 2 = 1
+
+// 1.7 ++ operator Increment (Povecava vrednost za 1);
+// postfiksni operator
+let x = 5;
+x++; // x = x + 1
+console.log(x);
+// console.log(5 ++); // 5 = 5+1 sto nema veze sa vezom
+
+let y = 11;
+// prefiksni operator
+console.log(++y);
+
+// 1.8  -- operator Decrement (Smanjuje vrednost za 1);
+let z = 99;
+z--; // z = z-1
+// postfiksni perator
+console.log(z);
+// prefiksni operator
+let w = 65;
+console.log(--w);
+
+
+
+
+
+
+
+
+
+
+
+
