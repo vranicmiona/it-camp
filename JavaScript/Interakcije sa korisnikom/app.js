6eb2397f232e4895a2ba941851a509bca5d6d6d6
vranicmiona <111905437+vranicmiona@users.noreply.github.com>
2022-10-25 21:09:20 +0200

    Interakcije i kondicionali

---
+++ JavaScript/Interakcije sa korisnikom/app.js
@@ -0,0 +1,48 @@
+// INTERAKCIJE SA KORISNIKOM //
+
+// prompt() metoda nam sluzi za komunikaciju sa korisnikom.
+
+const ime = prompt("Unesite vase ime: ");
+console.log(ime);
+
+// Sve sto korisnik unese je typa string
+const godine = prompt("Unesite svoju starost: ");
+console.log(typeof godine);
+// Sada je nepohodno prevesti godine u broj:
+
+// Postoje 2 nacina za prevodjdenje strnga u broj:
+// 1. Number(variabla)
+const brGodina = Number(godine);
+console.log(brGodina);
+console.log(typeof brGodina);
+
+// 2. +variabla
+const brGodina2 = +godine;
+console.log(brGodina2);
+console.log(typeof brGodina2);
+
+const visina = +prompt("Unesite svoju visinu u cm: ");
+console.log(visina);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
