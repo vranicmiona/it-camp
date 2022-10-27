c5ee2dc2c5b4350bc418ce24bbc7b8aad1b8e7bf
vranicmiona <111905437+vranicmiona@users.noreply.github.com>
2022-10-27 19:20:14 +0200

    Domaci

---
+++ JavaScript/Interakcije sa korisnikom/domaci.js
@@ -0,0 +1,21 @@
+let a = +prompt("Unesite prvi broj: ");
+let b = +prompt("Unesite drugi broj: ");
+
+if(a == b){
+let kvadrat = a**2;
+console.log(kvadrat);
+}else if(a != b){
+let pravougaonik = 2*(a + b);
+console.log(pravougaonik); 
+}else{
+    console.log("Niste uneli broj: ");
+}
+
+let x = +prompt("Unesite prvi broj: ");
+let y = +prompt("Unesite drugi broj: ");
+if(y !== 0){
+    let kolocnik = x/y;
+    console.log(kolocnik); 
+}else{
+    console.log("Deljenje nije moguce: ");
+}
