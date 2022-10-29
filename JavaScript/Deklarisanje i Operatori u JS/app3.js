2677a3b9b0121bc995cf3db989cf9dde1a23cc15
vranicmiona <111905437+vranicmiona@users.noreply.github.com>
2022-10-29 18:30:38 +0200

    homework

--- JavaScript/Deklarisanje i Operatori u JS/app3.js
+++ JavaScript/Deklarisanje i Operatori u JS/app3.js
@@ -4,17 +4,17 @@ for (let c = 1; c <= 20; c += 2) {
 }
 // Ispisati sve brojeve od 50 do 100 koji su deljivi sa 5
 for (let b = 50; b <= 100; b++) {
-    if (b % 5 === 0){
+    if (b % 5 === 0){ // odtatak pri deljenju tog broja sa 5 treba da bude 0
     console.log(b);}
 }
 // Ispisati dvostruku vrednost prirodnih brojeva od 6 do 14
 for (let a = 6; a <= 14; a++) {
-  console.log(a*2);
+  console.log(a**2); // ne treba uslov  
 }
-// Ipisati sumu neaprinih prirodnih brojeva od 10 do 20
+// Ipisati sumu neparnih prirodnih brojeva od 10 do 20
 let sum = 0;
-for (q = 10; q <= 20; q++){
-    if(sum = sum + q) {
-        console.log(q);
-    }
+for (let i = 10; i < 21; i++){
+if (i % 2 !== 0){
+sum += i;
 }
+}console.log(sum); // uradila sam i sama pre na slican nacin
