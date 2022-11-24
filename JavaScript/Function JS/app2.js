1663d3524b27ed302dc78a458d428787484d83e6
vranicmiona <111905437+vranicmiona@users.noreply.github.com>
2022-11-24 20:18:41 +0100

    Function JS

--- JavaScript/Function JS/app2.js
+++ JavaScript/Function JS/app2.js
@@ -6,3 +6,50 @@ if (NaN(a)){
     return "Kavdrat broja iznosi " + a**2
 }} 
 console.log = (kvadtarBroja(+prompt("Uneiste broj.")));
+
+// 2. Arrow function:
+// const myFunction = () => {
+    // function code}
+    const proizvodDvaBroja = (br1, br2) => {
+        return br1 * br2;
+    };
+    console,log(proizvodDvaBroja(2, 16));
+    // Ako Arrow funkcija iam samo jedan parametar nije neophodno okrtuziti ga zagradama 
+    // Ako je funkcija jednostavna, tj. ako odmah varac neki jednostavn izraz onda sama sintaksa izgleda kraca
+    // zapravo nije neophodno koristiti {} ili return keyword
+    const kvadratBroja = broj => broj ** 2;
+
+    const funkcija = () => { // nemamo parametar () jer korisnik unosi +prompt...
+        let broj = +prompt("Uneti broj:");
+      if(isNaN(broj)){ return "Moraju biti uneti brojevi: "
+    } else if(broj > 0) {
+        return "UNET JE POZITIVAN BROJ"
+    } else if (broj < 0){
+       return "UNET JE NEGATIVAN BROJ"
+    } else {
+        return "UNETA JE NULA"
+    }
+    }; console.log(funkcija()); // neka se izvrsi ta i ta funkcija
+
+    const fun = () => {
+        let satnica = +prompt("Uneti satnicu: ");
+        let minuti = +prompt("Uneti minute: ");
+        if(satnica >= 9 && satnica < 17) {
+            return "da";
+        } else if(minuti >= 0 && minuti < 59){
+        return "da";
+        } else{
+            "Niste uneli odgovarajuce brojeve"
+        }
+    }; console.log(fun());
+
+    const mail = function(satnica, minuti) {
+        if(satnica >= 9 && satnica < 17 && minuti >= 0 && minuti < 60){
+            return "Mail je stigao u toku radnog vremena."
+        } else{
+            return "Mail nije stigao u toku radnog vremena."
+        }
+    }; console.log(mail(4, 39));
+       console.log(mail(14, 39));
+       console.log(mail(9, 90));
+ 
