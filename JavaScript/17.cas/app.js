9ddaae8661b5349de3c598abbb897d0ee0ac49db
vranicmiona <111905437+vranicmiona@users.noreply.github.com>
2022-12-01 20:51:54 +0100

    Method

---
+++ JavaScript/17.cas/app.js
@@ -0,0 +1,18 @@
+// var
+// let
+// const
+// bez ista
+// HOISTING // 
+
+// Hoisting predstavlja deklarisanje promenljivih pre same dodele vrednosti(u pozadini).
+// Prilikom citanja koda se "izvlaci" deklaracija na vrhu.
+prom1 = 23;
+console.log(prom1);
+
+// var prom1; mozemo izvrsiti naknadnu deklaraciju vec koriscenje promenljive preko var keyword (a i ne mora jer JavaScript to radi u pozadini prilikom citanja koda).
+
+// let prom1; kasnije deklarisanje promenljive preko let keyword nije moguce.
+// const prom1; mora odjednom u istoj linijji da se izvrsi inicijalizacija i deklaracija;
+// kasnije deklarisanje promenljive preko let keyword nije moguce.
+
+
---
+++ JavaScript/17.cas/app2.js
@@ -0,0 +1,25 @@
+// trim() m;etoda brise rzmake sa obe strane stringa.
+const recenica = "    Da li vam je hladno?    ";
+console.log(recenica);
+const recenica2 = recenica.trim();
+console.log(recenica2);
+console.log(recenica2.length);
+
+// Postoje 3 metode za ekstraktovanje (uzimanje) jednog karaktera nekog stringa:
+// 1. chatAt(index) - vraca nam karakter nekoga stringa sa poslatim indexom.
+// 2. charCodeAt(index) - vraca nam kod karaktera nekog stringa za poslatim indexom.
+// 3. string[index] - vrlo slicno charAt metodi (sa jednom razlikom)
+
+const novaRecenica = "Spava mi se."
+const char1 = novaRecenica.charAt(3);
+console.log(char1);
+const char2 = novaRecenica.charCodeAt(3); // retko ce se primenjivati
+console.log(char2);
+
+const char3 = novaRecenica[3];
+console.log(char3);
+
+const char33 = novaRecenica.charCodeAt(46); // u consolei se ispisuje prazan string ""
+console.log(char33);
+const char46 = novaRecenica[33];
+console.log(char46); // u consoli se ispisuje undefined
