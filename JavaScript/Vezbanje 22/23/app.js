95dd8d729986d6bcb4eef6008c579a4fb6417ea4
vranicmiona <111905437+vranicmiona@users.noreply.github.com>
2022-12-14 16:18:48 +0100

    #update

--- JavaScript/Vezbanje 22/23/app.js
+++ JavaScript/Vezbanje 22/23/app.js
@@ -1,41 +1,49 @@
-// 1. 
+// 1.
 const today = new Date();
 const day = today.getDay();
-const daylist = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const daylist = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 console.log("Today is: " + daylist[day] + ".");
 let hours = today.getHours();
 let minutes = today.getMinutes();
 let seconds = today.getSeconds();
-let prepand = (hours >= 12) ? " PM ":" AM ";
-hours = (hours >= 12) ? hours - 12 : hours;
-if(hours === 0 && hours === "PM" && minutes === 0 && seconds === 0){
-    hours = 12;
-    prepand = "NOON";
-} else if(hours === 0 && hours === "AM" && minutes === 0 && seconds === 0){
-    hours = 12;
-    prepand = "MIDNIGHT"
+let prepand = hours >= 12 ? " PM " : " AM ";
+hours = hours >= 12 ? hours - 12 : hours;
+if (hours === 0 && hours === "PM" && minutes === 0 && seconds === 0) {
+  hours = 12;
+  prepand = "NOON";
+} else if (hours === 0 && hours === "AM" && minutes === 0 && seconds === 0) {
+  hours = 12;
+  prepand = "MIDNIGHT";
 } else {
-    hours = 12;
-    prepand = "AM"
+  hours = 12;
+  prepand = "AM";
 }
 console.log("Current time: " + hours + prepand + ":" + minutes + ":" + seconds);
 
 // 2.
 function print_current_page() {
-window.print();
-}  
+  window.print();
+}
 
 // 3.
 let today1 = new Date();
 let dd = today1.getDate();
 let mm = today1.getMonth();
 let yyyy = today1.getFullYear();
-if(dd < 10){
-    dd = "0" + dd;
-    if (mm < 10){
-        mm = "0" + mm;
-    }
-} 
+if (dd < 10) {
+  dd = "0" + dd;
+  if (mm < 10) {
+    mm = "0" + mm;
+  }
+}
 today1 = mm + "-" + dd + "-" + yyyy;
 console.log(today1);
 today1 = mm + "/" + dd + "/" + yyyy;
@@ -50,5 +58,5 @@ const a = 3;
 const b = 4;
 const c = 5;
 const s = (a + b + c) / 2;
-const total = Math.sqrt(s * ((s - a) + (s - b) + (s - c)));
+const total = Math.sqrt(s * (s - a + (s - b) + (s - c)));
 console.log(total);
