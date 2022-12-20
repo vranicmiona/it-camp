52983c04625965fbe6ee38bab86df29f8cedea5f
vranicmiona <111905437+vranicmiona@users.noreply.github.com>
2022-12-20 20:18:39 +0100

    Number method

--- JavaScript/Number method/app.js
+++ JavaScript/Number method/app.js
@@ -10,13 +10,13 @@ console.log(y);
 console.log(typeof y); // string
 console.log(x + y);
 
-// toExponetional() - nam vraca string broja, zaokruzen na onoliko decimala, koliki je argumen metode,
+// toExponetional() - nam vraca string broja, zaokruzen na onoliko decimala, koliki je argument metode,
 let z = 55.4758;
 let w = z.toExponential(2);
 console.log(w);
 console.log(typeof w); // string
 
-// toFixed() - metoda nam vraca string broja, koji ce biti zaokruzen n onoliko deecimala koliko je zapravo argument.
+// toFixed() - metoda nam vraca string broja, koji ce biti zaokruzen na onoliko decimala koliko je zapravo argument.
 a = 12.1234;
 b = +a.toFixed(2);
 console.log(b);
@@ -64,26 +64,23 @@ console.log(izbrisiDruguCifru(234));
 console.log(izbrisiDruguCifru(6));
 
 // Domaci zadatak:
-// Napraviti funkciju koja ocekuje cetvorocifren broj, a vraca broj kome su zamenjene prva i treca cifra i druga i cetvrta cifra. 4752 5247
+// Napraviti funkciju koja ocekuje cetvorocifren broj, a vraca broj kome su zamenjene prva i treca cifra i druga i cetvrta cifra. 4752 5247 Rdi se tako sto uzmemo ostatak pri deljenju i odvajamo brojeve
 const mojaFunkcija = (cetvorocifrenBroj) => {
   const stringBrojaa = cetvorocifrenBroj.toString();
-  const duzina = stringBrojaa.length;
+  const duzina = stringBrojaa.length / 2;
   let noviString = "";
-  for (let i = 0; i < duzina; i++) {
-    if (duzina % 2 === 1 && i === duzina - 1) {
-      noviString += stringBrojaa[i];
-    } else if (duzina !== 4) {
-      return "Mora sadrzati cetiri cifre!";
-    } else if (i % 2 === 0) {
-      noviString += stringBrojaa[i + 1];
-    } else if (i % 2 === 1) {
-      noviString += stringBrojaa[i - 1];
-    }
+  if (stringBrojaa.length !== 4) {
+    return "Mora sadrzati cetiri cifre!";
   }
-  return noviString;
+  const prvaPolovina = stringBrojaa.slice(0, duzina); // (0, 2)
+  const drugaPolovina = stringBrojaa.slice(duzina, stringBrojaa.length); // (2)
+  const recenica = noviString.concat(drugaPolovina, prvaPolovina); // drugaPolovina.concat(prviDeo)
+  return +recenica; // zbog plusa nema nule na primerima
 };
+
 console.log(mojaFunkcija(1234));
 console.log(mojaFunkcija(123));
+console.log(mojaFunkcija(1203));
 
 // Global JavaScript methods //
 
@@ -107,3 +104,37 @@ console.log(Number(true)); // 1
 console.log(Number(false)); // 0
 console.log(Number([])); // 0
 console.log(Number({})); // NaN
+
+// 2. parseInt() - Analizira argument i vraca nam ceo broj ako je moguce.
+console.log(parseInt("23")); // 23
+console.log(typeof parseInt("23")); // number
+// Krajnji razmaci ne prave problem
+console.log(parseInt(" 23         ")); // 23
+console.log(parseInt("23+5")); // 23 posle se pojavljuje neki znak koji je nista i ja uzimam sve do toga broja, i dozvoljava da stavimo uz broj nesto sto nije broj
+
+// Razmaci jesu dozvoljeni kod parseInt() metode. S tim sto se uzima u obzir samo prvi broj koji se pojavljuje.
+console.log(parseInt("23 56")); // 23
+console.log(parseInt("ahsuh23 56")); // NaN
+console.log(parseInt("23.56")); // 23 funkcionise kao Math.trunc() odstranjuje decimalni ostatak
+console.log(parseInt("23,78")); // 23
+console.log(parseInt("asjhgs")); // NaN
+console.log(parseInt(true)); // NaN
+console.log(parseInt(false)); // NaN
+console.log(parseInt([])); // NaN
+console.log(parseInt({})); // NaN
+
+// 2. parseFloat() - analizira argumet i vraca realan broj sa decimalnim zapisom ako je moguce.
+console.log(parseFloat("23")); // 23
+console.log(typeof parseFloat("23")); // number
+// Krajnji razmaci ne prave problem
+console.log(parseFloat(" 23         ")); // 23
+console.log(parseFloat("23+5")); // NaN
+// Razmaci nisu dozvoljeni kod parseFloat metode
+console.log(parseFloat("23 56")); // NaN
+console.log(parseFloat("23.56")); // 23.56
+console.log(parseFloat("23,78")); // NaN
+console.log(parseFloat("asjhgs")); // NaN
+console.log(parseFloat(true)); // 1
+console.log(parseFloat(false)); // 0
+console.log(parseFloat([])); // 0
+console.log(parseFloat({})); // NaN
