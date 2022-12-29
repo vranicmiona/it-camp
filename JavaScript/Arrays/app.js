f53b2a271fdaa8cfa7d8622b8cde646ce7937111
vranicmiona <111905437+vranicmiona@users.noreply.github.com>
2022-12-29 21:02:12 +0100

    Date & Arrays

---
+++ JavaScript/Arrays/app.js
@@ -0,0 +1,22 @@
+// 1. Osnovni Tipovi Podataka
+// (Primitivni Tipovi Podataka)
+// (Vrednosni Tipovi Podataka) mogu da sadrze samo jednu vrednost
+
+// 2. Referentni tipovi podataka skladiste dosta vrednosti
+// Neprimitivni Tipovi Podataka
+
+let brojGodina = 26;
+let brojTvojihGodiina = brojGodina;
+console.log(brojTvojihGodiina); // 26
+
+brojTvojihGodiina = 22;
+console.log(brojTvojihGodiina); // 22
+console.log(brojGodina); // 26
+/////////////////////////////////////////////////
+let godine = [20, 21, 18, 17, 17, 19, 18, 19];
+let noveGodine = godine;
+console.log(noveGodine);
+noveGodine = [21, 22, 19, 19, 18, 18, 20, 20];
+noveGodine.push(34);
+console.log(noveGodine);
+console.log(godine); // menjanjem jedne menja se i ona druga, kod vrednosnih nema menjajnja
