833f9d33f4f9f3bda1d19fc203f2383d6f046613
vranicmiona <111905437+vranicmiona@users.noreply.github.com>
2023-01-14 13:43:59 +0100

    Callback function; forEach() method; map() method

---
+++ JavaScript/Callback function/app.js
@@ -0,0 +1,92 @@
+// callback function //
+
+// callback funkcija predstvalja funkciju koju saljemo kao arument neke druge funkcije.
+// Sam argument ne treba da bude izvrsavanje callback funkcije vec referenca na datu radnju
+
+function glavnaFunkcija(func) {
+  console.log("Radnja unutar glavne funkcije.");
+  func();
+  console.log("Kraj glavne funkcije.");
+}
+function callback() {
+  console.log("Radnja unutar callback fnukcije.");
+}
+glavnaFunkcija(callback);
+
+// Napraviti glavnu funkciju unutar koje ce se desiti ispis sta ona predstavlja, a celu stvar ce da izvrsi callback funkcija. Callback funkcija ima 2 parametra, i ispisuje rezultat par1 stepenovan par2.
+function mainFunction(callBack2) {
+  console.log("Prvi broj stepenovan drugim je: ");
+  callBack2(2, 3);
+}
+const callBack2 = (par1, par2) => {
+  console.log(par1 ** par2);
+};
+mainFunction(callBack2);
+
+// JavaScript Array Iteration //
+
+// Array Iteration metode proaze kroz svvaki element niza.
+// forEach() metoda poziva callback funkciju za svaki element niza.
+
+const niz = ["tarik", "hamed", "emin", "bakir", "dzenan"];
+
+// forEach() metoda moze imati 3 parametra:
+// 1. element niza
+// 2. index niza
+// 3. ceo niz
+
+niz.forEach((element) => {
+  console.log(element);
+});
+
+niz.forEach((element, index) => {
+  console.log(`${element} se nalazi na ${index + 1} mestu u nizu.`);
+});
+
+niz.forEach((element, index, arr) => {
+  console.log(
+    `${element} se nalazi na ${index + 1} mestu u nizu, a ceo niz je ${arr}`
+  );
+});
+
+// Ne moramo koristiti nazive od pre:
+niz.forEach((student, pozicija) => {
+  console.log(`${student} se nalazi na poziciji ${pozicija + 1}`);
+});
+
+// map() metoda kreira novi niz (iste duzine kao orignalni) sa logikom postavljnom unutar callback funkcije unuta date metode.
+
+// Preko petlje napraviti novi niz koji ce imati sve elemente originalnog niza.
+
+const niz2 = [];
+for (let student of niz) {
+  niz2.push(student);
+}
+console.log(niz2);
+
+// const niz3 = niz.map(function (student) {
+//   return student;
+// });
+
+const niz3 = niz.map((student) => student);
+console.log(niz3);
+
+// Preko map() metode napraviti novi niz gde ce imena svih studenata pocinjati velikim slovom.
+
+const niz4 = niz.map((student) => student[0].toUpperCase() + student.slice(1));
+console.log(niz4);
+
+// 2. Write a JavaScript program which accept a number as input and insert dashes (-) between each two even numbers. For example if you accept 025468 the output should be 0-254-6-8
+
+const addSomething = (num) => {
+  let newNum = "";
+  for (let i = 0; i < niz.length; i++) {
+    if (num[i] % 2 === 0 && num[i + 1] % 2 === 0) {
+      newNum += num[i] + "-";
+    } else {
+      newNum += num[i];
+    }
+  }
+  return newNum;
+};
+console.log(addSomething("025468"));
---
+++ JavaScript/Callback function/app2.js
@@ -0,0 +1,42 @@
+// Domaci:
+// 1. Pomocu map() metode napraviti novi niz stringova ciji ce elementi da budu
+// ispisani velikim slovima.
+
+// 2. Napraviti novi niz koji sadrzi kvadrate elemenata postojeceg niza:
+
+// 3. Napraviti novi niz koji sadrzi kvadratne korene elemenata postojeceg niza.
+
+// 4.
+// const brojevi = [-5, 4, 3, 10, 14, -5];
+// Napraviti novi niz koji ce sve negativne brojeve pomnoziti sa (-1), a pozitivne kvadrirati.
+
+// 1.
+const niz = [
+  "analiticka hemija",
+  "fizicka hemija",
+  "organska hemija",
+  "opsta hemija",
+  "neorgansak hemija",
+];
+const niz1 = niz.map(
+  (predmeti) => predmeti[0].toUpperCase() + predmeti.slice(1)
+);
+console.log(niz1);
+
+// 2.
+const num = [2, 8, 5, 10, 22, 12];
+const num2 = num.map((broj) => broj ** 2);
+console.log(num2);
+
+// 3.
+const sqrt = [4, 64, 25, 100, 484, 144];
+const sqrt2 = sqrt.map((broj) => Math.sqrt(broj));
+console.log(sqrt2);
+
+// 4.
+const brojevi = [-5, 4, 3, 10, 14, -5];
+const brojevi2 = brojevi.map(function (broj) {
+  const izracunaj = broj < 0 ? broj * -1 : broj ** 2;
+  return izracunaj;
+});
+console.log(brojevi2);
