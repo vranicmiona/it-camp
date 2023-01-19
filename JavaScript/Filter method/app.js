20c23a4b7cb6e3c04541420372fddecf42051b2e
vranicmiona <111905437+vranicmiona@users.noreply.github.com>
2023-01-19 12:06:48 +0100

    #homework

--- JavaScript/Filter method/app.js
+++ JavaScript/Filter method/app.js
@@ -123,10 +123,26 @@ console.log(numbers([9, 8, 4, 0, -6, 2, 22, 32]));
 
 // 3.
 function frequentItem(array) {
-  let count = 0;
-  const array2 = array.filter((element, index) => {
-    if (element === index) {
-      count++;
+  if (array.length == 0) return null;
+  var modeMap = {};
+  var maxEl = array[0],
+    maxCount = 1;
+  for (var i = 0; i < array.length; i++) {
+    var el = array[i];
+    if (modeMap[el] == null) modeMap[el] = 1;
+    else modeMap[el]++;
+    if (modeMap[el] > maxCount) {
+      maxEl = el;
+      maxCount = modeMap[el];
     }
-  });
+  }
+  return maxEl;
 }
+console.log(frequentItem([3, "a", "a", "a", 2, 3, "a", 3, "a", 2, 4, 9, 3]));
+
+const s = {};
+arr1.map((element) => {
+  s[element] = s[element] + 1 || 1;
+});
+const najveci = Math.max(s);
+console.log();
