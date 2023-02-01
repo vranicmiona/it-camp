ae9749f6d48e291916a655cd697a28155bd05c74
vranicmiona <111905437+vranicmiona@users.noreply.github.com>
2023-02-01 16:57:29 +0100

    Object bulit in methods

--- JavaScript/42. cas/app.js
+++ JavaScript/42. cas/app.js
@@ -87,22 +87,22 @@ const newArr2 = godinee.filter((element) => element.starost > 17);
 console.log(newArr2);
 
 // Domaci:
-// 1.
 
 // 2. Write a JavaScript program to sort an array of JavaScript objects.
-// var library = [
-//     {
-//         title:  'The Road Ahead',
-//         author: 'Bill Gates',
-//         libraryID: 1254
-//     },
-//     {
-//         title: 'Walter Isaacson',
-//         author: 'Steve Jobs',
-//         libraryID: 4264
-//     },
-//     {
-//         title: 'Mockingjay: The Final Book of The Hunger Games',
-//         author: 'Suzanne Collins',
-//         libraryID: 3245
-//     }];
+var library = [
+  {
+    title: "The Road Ahead",
+    author: "Bill Gates",
+    libraryID: 1254,
+  },
+  {
+    title: "Walter Isaacson",
+    author: "Steve Jobs",
+    libraryID: 4264,
+  },
+  {
+    title: "Mockingjay: The Final Book of The Hunger Games",
+    author: "Suzanne Collins",
+    libraryID: 3245,
+  },
+];
