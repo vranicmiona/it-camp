c0d4fd5d4a6699c5833b545d2bfc07acffc750f1
vranicmiona <111905437+vranicmiona@users.noreply.github.com>
2023-02-07 19:04:57 +0100

    Tasks

---
+++ JavaScript/Vezbanje zadataka/app2.js
@@ -0,0 +1,84 @@
+// 1.	(20) Uneti string se šifruje tako što se zamenjuje redosled znacima u stringu. Prvi i drugi zamene mesta, zatim 3. i 4. zamene mesta itd. Npr. za string 'Pera ima devojku', treba dobiti 'ePari amd vejouk'. Ako string ima neparan broj znakova, poslednji znak se ne dira.
+function zamenaMesta(string) {
+  let newString = "";
+  for (let i = 0; i < string.length; i++) {
+    if (string.length % 2 === 0 && string.length % 2 !== 0) {
+      newString += string.length[i];
+    } else if (i % 2 === 0) {
+      newString += string[i + 1];
+    } else if (i % 2 !== 0) {
+      newString += string[i - 1];
+    }
+  }
+  return newString;
+}
+console.log(zamenaMesta("Pera ima devojku"));
+
+// 2.	(20) Napraviti pomocni niz koji ce sadrzati elemente od oba niza. Napraviti jos jedan pomocni niz koji ce iz prvog dodatnog niza uzeti parne brojeve. U drugom pomocnom nizu umetnuti na prva dva mesta 10 i 20.
+// Funkcija na kraju treba da vrati dva napravljena pomocna niza.
+DATA1 = [2, 26, 38, 75, 11, 29];
+DATA2 = ["a", "b", "c", "d", "e"];
+
+function main(DATA1, DATA2) {
+  const pomocniNiz1 = DATA1.concat(DATA2);
+  const pomocniNiz2 = pomocniNiz1.filter((element) => element % 2 === 0);
+  pomocniNiz2.unshift(10, 20);
+  return `Prvi pomocni niz je ${pomocniNiz1}, a drugi pomcni niz je ${pomocniNiz2}.`;
+}
+console.log(main([2, 26, 38, 75, 11, 29], ["a", "b", "c", "d", "e"]));
+
+// 3.	(20) Shopping Cart.
+// There are array of objects with all products that someone bought. In product object we got name, price per unit, and amount of units. Your job is to loop throught array, and make new array of objects with two key-value pairs name and total price for that product.
+// After you did the job with new array, you need to sum all prices of every product and return total sum value, and add a FIXED 320 shipping costs.
+// After all of that you should print in console following statement:
+// 'You got ${num of products} in cart, shipping cost is ${shipping cost}, your total is ${total}.'
+
+const CART_DATA = [
+  {
+    id: 1,
+    name: "Hawai Shirt",
+    price: 30,
+    amount: 2,
+    categorty: "summer",
+    rating: 4.5,
+  },
+  {
+    id: 3,
+    name: "Adidas Slippers",
+    price: 35,
+    amount: 1,
+    categorty: "summer",
+    rating: 4.5,
+  },
+  {
+    id: 6,
+    name: "White T-Shirt",
+    price: 15,
+    amount: 4,
+    categorty: "summer",
+    rating: 4.7,
+  },
+  {
+    id: 4,
+    name: "Summer hat",
+    price: 7.5,
+    amount: 3,
+    categorty: "summer",
+    rating: 40,
+  },
+];
+
+function totalPrice(arr) {
+  const shippingCosts = 320;
+  const newArr = arr.map((element) => {
+    return {
+      name: element.name,
+      totalprice: element.price * element.amount,
+    };
+  });
+  const tPrice = newArr.map((element) => element.totalprice);
+  const allPrice = tPrice.reduce((previous, current) => previous + current);
+  const addShippingCosts = allPrice + shippingCosts;
+  return `You got ${arr.length} in cart, shipping cost is ${shippingCosts}, your total price is ${addShippingCosts}`;
+}
+console.log(totalPrice(CART_DATA)); // 4, 320, 497.5
