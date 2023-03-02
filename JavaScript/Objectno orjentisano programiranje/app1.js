c374fa3ad0c7871e15ee9160431daf0d57abb546
vranicmiona <111905437+vranicmiona@users.noreply.github.com>
2023-03-02 20:59:36 +0100

    Objektno orjentisanje

---
+++ JavaScript/Objectno orjentisano programiranje/app1.js
@@ -0,0 +1,98 @@
+// Sve funkcionise preko klasa i obj
+
+// function User(ime, prezime) { // preko funk User je napravljen objekat
+//   // argument, preko kojeg pristupamo prop
+//   this.ime = ime;
+//   this.prezime = prezime;
+// }
+// const bakir = new User("Bakir", "Ujkanovic");
+// console.log(bakir);
+///////////////// DEFINISANJE PROP PREKO KLASE
+// class Person {
+// mozemo da definisemo prop koji izlaze iz class-e
+//   ime = "Default"; // moze da ima defaultnu vrednost
+//   prezime;
+//   godine; // prop od tog obj
+
+// keyword u JS, ime, prezime nam je takodje konstruktor, za pravljenje nekih objekata
+//   constructor(ime, prezime, brGodina) {
+//     this.ime = ime; // instance te klase
+//     this.prezime = prezime;
+//     this.godine = brGodina;
+//   }
+//   imeIPrezime() {
+//     return `${this.ime} ${this.prezime}`;
+//   }
+// }
+// const person1 = new Person("Mehmed", "Kucevic", 15);
+// // person1 moze pristupiti bilo kojoj metodi iz klase Person(koja je naprvaljena na standardan nacin)
+// console.log(person1);
+
+class Product {
+  static IncreaseForTen = 1.1;
+  constructor(name, price, decription, weight) {
+    // sluzi za pravljenje obj,
+    this.name = name;
+    this.price = price;
+    this.decr = decription;
+    this.weight = weight;
+  }
+  static increasePrice() {
+    console.log(
+      `This method will increase price for our product for 10% which will be multiplay current price buy ${this.IncreaseForTen}`
+    );
+  }
+}
+// Ne mozemo pristupiti statickoj metodi iz objekta, vec samo direktno preko klase.
+// console.log(socks.IncreaseForTen);
+
+const socks = new Product("white socks", 120, "100% cotton", 50);
+console.log(socks);
+console.log(Product.IncreaseForTen);
+
+// INHERITANCE CLASS
+
+class Person {
+  constructor(ime, prezime, pol, starost) {
+    this.ime = ime;
+    this.prezime = prezime;
+    this.pol = pol;
+    this.starost = starost;
+  }
+
+  getInfo() {
+    console.log(
+      `${this.ime}, ${this.prezime} ima ${this.starost}, ${this.pol}`
+    );
+  }
+}
+
+const person = new Person("Mitar", "Vranic", "M", 20);
+console.log(person);
+console.log(person.getInfo());
+
+class Student extends Person {
+  // moze samo da ima jednu klasu od koje nasledjuje
+  constructor(
+    ime,
+    prezime,
+    starost,
+    pol,
+    brojIndexa,
+    godinaStudija,
+    prosecnaOcena
+  ) {
+    super(ime, prezime, starost, pol);
+    this.brojIndexa = brojIndexa;
+    this.godinaStudija = godinaStudija;
+    this.prosecnaOcena = prosecnaOcena;
+  }
+  getInfo() {
+    console.log(
+      `${this.ime} ${this.prezime} ima ${this.godina}. Trenutno je na ${this.godinaStudija} i prosecna ocena mu je ${this.prosecnaOcena}.`
+    );
+  }
+}
+const student = new Student("Amer", "Honic", "M", 19, 123321, 1, 7.77);
+console.log(student);
+console.log(student.getInfo());
