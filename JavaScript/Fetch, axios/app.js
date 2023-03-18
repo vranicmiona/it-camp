f337c10e6f9e92c24ce12590eee43b0f5f2b81b9
vranicmiona <111905437+vranicmiona@users.noreply.github.com>
2023-03-18 22:58:16 +0100

    Fetch,axios

--- JavaScript/Fetch, axios/app.js
+++ JavaScript/Fetch, axios/app.js
@@ -55,3 +55,12 @@ const getMapUsers = async () => {
 getMapUsers()
   .then((data) => console.log(data))
   .catch((error) => console.log(error));
+
+const getComments = () => {
+  axios
+    .get("https://jsonplaceholder.typicode.com/comments")
+    .then((response) => console.log(response.data))
+    .catch((error) => console.log(error.message));
+};
+
+getComments();
